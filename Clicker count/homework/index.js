e31b1685d366b8667fd04996be60d69c43e73802
Vladyslav Zaikovskyi <vladyslav.zaikovskyi@gmail.com>
2023-08-31 20:49:28 +0300

    mod

--- Clicker count/homework/index.js
+++ Clicker count/homework/index.js
@@ -1,30 +1,31 @@
-let clickMeButton = document.querySelector('.me');
+let clickMeButton = document.querySelector(".me");
 let counter = 0;
 let nicknameInput;
 let bestScoreNickname;
-const BEST_SCORE_ALL_TIME = 'bestScoreAllTime';
+const BEST_SCORE_ALL_TIME = "bestScoreAllTime";
 const bestScoreAllTime = JSON.stringify([]);
 const timerTime = 5000;
+ 
 
 if (localStorage.getItem(BEST_SCORE_ALL_TIME) === null) {
   localStorage.setItem(BEST_SCORE_ALL_TIME, bestScoreAllTime);
 }
 
-if (sessionStorage.getItem('bestScore') === null) {
-  sessionStorage.setItem('bestScore', 0);
+if (sessionStorage.getItem("bestScore") === null) {
+  sessionStorage.setItem("bestScore", 0);
 }
 
-clickMeButton.addEventListener('click', counterIncrease);
+clickMeButton.addEventListener("click", counterIncrease);
 
 function counterIncrease() {
   counter++;
 }
 
 function getNickname() {
-  nicknameInput = document.getElementById('nickname').value;
+  nicknameInput = document.getElementById("nickname").value;
   try {
     if (nicknameInput.length === 0) {
-      throw 'Empty nickname';
+      throw "Empty nickname";
     }
     const bestScoreAllTime = JSON.parse(
       localStorage.getItem(BEST_SCORE_ALL_TIME)
@@ -37,7 +38,7 @@ function getNickname() {
     if (!userChecker) {
       const userData = {
         nickname: nicknameInput,
-        score: 0
+        score: 0,
       };
       bestScoreAllTime.push(userData);
       const bestScoreAllTimeString = JSON.stringify(bestScoreAllTime);
@@ -54,8 +55,8 @@ function clickTimer() {
   counter = 0;
   setTimeout(() => {
     alert(`You clicked ${counter} times`);
-    if (counter > sessionStorage.getItem('bestScore')) {
-      sessionStorage.setItem('bestScore', counter);
+    if (counter > sessionStorage.getItem("bestScore")) {
+      sessionStorage.setItem("bestScore", counter);
 
       const bestScoreAllTime = JSON.parse(
         localStorage.getItem(BEST_SCORE_ALL_TIME)
@@ -79,33 +80,32 @@ function bestResultEver() {
     localStorage.getItem(BEST_SCORE_ALL_TIME)
   );
 
-  const {
-    nickname,
-    score
-  } = bestScoreAllTime.reduce((prev, current) =>
+  const { nickname, score } = bestScoreAllTime.reduce((prev, current) =>
     prev.score > current.score ? prev : current
   );
 
   alert(`Best result for the whole time is: ${score} by ${nickname}`);
 }
 
-let bestResultAllTimeMeButton = document.querySelector('.best-result-all-time');
-bestResultAllTimeMeButton.addEventListener('click', bestResultEver);
+let bestResultAllTimeMeButton = document.querySelector(".best-result-all-time");
+bestResultAllTimeMeButton.addEventListener("click", bestResultEver);
 
-let bestResultMeButton = document.querySelector('.best-result');
-bestResultMeButton.addEventListener('click', () => {
+let bestResultMeButton = document.querySelector(".best-result");
+bestResultMeButton.addEventListener("click", () => {
   alert(`Best result is: ${sessionStorage.bestScore}`);
 });
 
-const clearBestResultAllTime = document.querySelector('.clear-best-result-all-time');
-clearBestResultAllTime.addEventListener('click', () => {
+const clearBestResultAllTime = document.querySelector(
+  ".clear-best-result-all-time"
+);
+clearBestResultAllTime.addEventListener("click", () => {
   localStorage.setItem(BEST_SCORE_ALL_TIME, bestScoreAllTime);
 });
 
-const clearBestResult = document.querySelector('.clear-best-result');
-clearBestResult.addEventListener('click', () => {
-  sessionStorage.setItem('bestScore', 0);
+const clearBestResult = document.querySelector(".clear-best-result");
+clearBestResult.addEventListener("click", () => {
+  sessionStorage.setItem("bestScore", 0);
 });
 
-const btnStart = document.querySelector('.start');
-btnStart.addEventListener('click', getNickname);
+const btnStart = document.querySelector(".start");
+btnStart.addEventListener("click", getNickname);
